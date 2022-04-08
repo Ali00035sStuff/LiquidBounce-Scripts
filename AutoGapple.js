c3023076da7c7fe7c057e43afcdaa123107d8512
Ali <95705989+Ali00035v2@users.noreply.github.com>
2022-04-08 23:01:19 +0300

    Update AutoGapple.js

--- AutoGapple.js
+++ AutoGapple.js
@@ -1,80 +1,66 @@
 ///api_version=2
-//Copyright 2020 commandblock2 distributed under AGPL-3.0-or-later
 (script = registerScript({
     name: "AutoGapple",
-    version: "1.0",
-    authors: ["LolMc","commandblock2"]
-})).import("Core.lib")
+    authors: ["commandblock2", "yorik100", "CzechHek"],
+    version: "2.3"
+})).import("Core.lib");
 
+var timer = new MSTimer(), Potion = Java.type('net.minecraft.potion.Potion');
 
-Potion = Java.type('net.minecraft.potion.Potion')
-
-var originalIndex = -1
-var inventoryIndex = -1
-module =
-{
-    name: "AutoGapple",
-    description: "Eat gapple when your health is low",
-    category: "combat",
-    values:
-        [
-            health = value.createFloat("Health", 10, 1, 20),
-            itemSwitchDelay = value.createInteger("SwitchDelayms", 100, 0, 1000)
-        ],
-
+module = {
+    description: "Eats golden apple when your health is low.",
+    category: "Player",
+    values: [
+        health = value.createFloat("Health", 10, 1, 20),
+        itemswitchdelay = value.createInteger("Delay", 100, 0, 1000),
+        regencheck = value.createBoolean("RegenCheck", false),
+        absorptioncheck = value.createBoolean("AbsorptionCheck", false),
+        chestcheck = value.createBoolean("ChestCheck", false),
+        inventorycheck = value.createBoolean("InventoryCheck", false),
+        changeonetick = value.createBoolean("ChangeOneTick", false)
+    ],
+    onEnable: function () {
+        isChanging = gAppleIndex = null;
+        changeTicks = 0;
+    },
     onUpdate: function () {
-        if (mc.thePlayer.getHealth() <= health.get() && !mc.thePlayer.isPotionActive(Potion.regeneration)) {
-            gAppleIndex = InventoryUtils.findItem(36, 45, Items.golden_apple)
-            gAppleIndex = gAppleIndex == -1 ? InventoryUtils.findItem(9, 36, Items.golden_apple) : gAppleIndex
-
-            if (originalIndex == -1) { // wtffffff have to check null because if (originalIndex) doesn't work when it equas to 0
-                originalIndex = mc.thePlayer.inventory.currentItem
+        if (changeTicks > 0) changeTicks++;
+        if (isChanging && changeTicks > 3) {
+            isChanging = !!(changeTicks = 0);
+            sendPacket(new C08PacketPlayerBlockPlacement(BlockPos.ORIGIN, 255, mc.thePlayer.inventory.mainInventory[gAppleIndex - 36], 0, 0, 0));
+            for (i = -1; ++i < 32;) sendPacket(new C03PacketPlayer(mc.thePlayer.onGround));
+            if (mc.thePlayer.inventory.currentItem != gAppleIndex) sendPacket(new C09PacketHeldItemChange(mc.thePlayer.inventory.currentItem));
+            if (KillAuraModule.blockingStatus || mc.thePlayer.isBlocking()) sendPacket(new C08PacketPlayerBlockPlacement(BlockPos.ORIGIN, 255, mc.thePlayer.inventory.getCurrentItem(), 0, 0, 0));
+        }
+        if (mc.thePlayer.getHealth() > 0 && mc.thePlayer.getHealth() <= health.get()) {
+            gAppleIndex = InventoryUtils.findItem(36, 45, Items.golden_apple);
+            gAppleIndex = ~gAppleIndex ? gAppleIndex : InventoryUtils.findItem(9, 36, Items.golden_apple);
+            if (~gAppleIndex && (gAppleIndex - 36 | 44 - gAppleIndex) >= 0 && timer.hasTimePassed(itemswitchdelay.get()) && (!(mc.currentScreen instanceof GuiChest) || !chestcheck.get()) && (!(mc.currentScreen instanceof GuiInventory) || !inventorycheck.get()) && (mc.thePlayer.getAbsorptionAmount() <= 0 || !absorptioncheck.get()) && (!mc.thePlayer.isPotionActive(Potion.regeneration) || !regencheck.get())) {
+                if (mc.thePlayer.inventory.currentItem != gAppleIndex) sendPacket(new C09PacketHeldItemChange(gAppleIndex - 36));
+                if (!changeonetick.get()) {
+                    sendPacket(new C08PacketPlayerBlockPlacement(BlockPos.ORIGIN, 255, mc.thePlayer.inventory.mainInventory[gAppleIndex - 36], 0, 0, 0));
+                    for (i = -1; ++i < 32;) sendPacket(new C03PacketPlayer(mc.thePlayer.onGround));
+                    if (mc.thePlayer.inventory.currentItem != gAppleIndex && !changeonetick.get()) sendPacket(new C09PacketHeldItemChange(mc.thePlayer.inventory.currentItem));
+                    if (KillAuraModule.blockingStatus || mc.thePlayer.isBlocking()) sendPacket(new C08PacketPlayerBlockPlacement(BlockPos.ORIGIN, 255, mc.thePlayer.inventory.getCurrentItem(), 0, 0, 0));
+                } else isChanging = !!(changeTicks = 1);
+                timer.reset();
+            } else if ((gAppleIndex - 9 | 35 - gAppleIndex) >= 0 && InventoryUtils.hasSpaceHotbar()) {
+                if (!(mc.currentScreen instanceof GuiInventory)) sendPacket(new C16PacketClientStatus(C16PacketClientStatus.EnumState.OPEN_INVENTORY_ACHIEVEMENT));
+                mc.playerController.windowClick(0, gAppleIndex, 0, 1, mc.thePlayer);
+                if (!(mc.currentScreen instanceof GuiInventory)) sendPacket(new C0DPacketCloseWindow());
             }
-
-
-            if (gAppleIndex >= 36 && gAppleIndex < 45) {
-                //switch to gapple
-                mc.thePlayer.inventory.currentItem = gAppleIndex - 36
-
-            } else if (gAppleIndex >= 9 && gAppleIndex < 36 && !(mc.currentScreen instanceof GuiInventory)) {
-                //switch gapple and 36 (first slot)
-                inventoryIndex = gAppleIndex
-                switchGapple(inventoryIndex)
-            } else return
-
-            mc.gameSettings.keyBindUseItem.pressed = true
-
         }
-        else {
-            reset()
+    },
+    onPacket: function (e) {
+        if (isChanging && e.getPacket() instanceof C09PacketHeldItemChange) e.cancelEvent();
+    },
+    onDisable: function () {
+        if (isChanging) {
+            isChanging = false;
+            sendPacket(new C08PacketPlayerBlockPlacement(BlockPos.ORIGIN, 255, mc.thePlayer.inventory.mainInventory[gAppleIndex - 36], 0, 0, 0));
+            for (i = -1; ++i < 32;) sendPacket(new C03PacketPlayer(mc.thePlayer.onGround));
+            if (mc.thePlayer.inventory.currentItem != gAppleIndex) sendPacket(new C09PacketHeldItemChange(mc.thePlayer.inventory.currentItem));
+            if (KillAuraModule.blockingStatus || mc.thePlayer.isBlocking()) sendPacket(new C08PacketPlayerBlockPlacement(BlockPos.ORIGIN, 255, mc.thePlayer.inventory.getCurrentItem(), 0, 0, 0));
         }
     }
-}
-function reset() {
-    if (originalIndex != -1 || inventoryIndex != -1)
-        mc.gameSettings.keyBindUseItem.pressed = false
-
-    if (originalIndex != -1 ) {
-        mc.thePlayer.inventory.currentItem = originalIndex;
-        originalIndex = -1
-    }
-
-    if (inventoryIndex != -1) {
-        switchGapple(inventoryIndex)
-        inventoryIndex = -1
-    }
-
-}
-
-function switchGapple(index) {
-    //open inventory (I don't care about when you are on horse, implement it yourself)
-    mc.getNetHandler().addToSendQueue(new C16PacketClientStatus(C16PacketClientStatus.EnumState.OPEN_INVENTORY_ACHIEVEMENT));
-    mc.displayGuiScreen(new GuiInventory(mc.thePlayer));
-
-    timeout(itemSwitchDelay.get(), function () {
-        slot = mc.thePlayer.inventoryContainer.getSlot(index)
-
-        mc.playerController.windowClick(mc.currentScreen.inventorySlots.windowId, slot.slotNumber, 0, 2, mc.thePlayer)
-        mc.thePlayer.closeScreen()
-    })
 }
